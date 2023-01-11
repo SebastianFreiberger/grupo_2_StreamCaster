1c0415482bdc30a8515a5f1f379d2fd7657480d9
SebaFrei <sebastianfreiberger03@gmail.com>
2023-01-11 10:38:34 -0300

    corrijo app.js con solo la linea necesaria para rutas

--- src/app.js
+++ src/app.js
@@ -10,11 +10,5 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.use('/', mainRouter);
-app.use('./registro', mainRouter);
-app.use('./login', mainRouter);
-app.use('./detalleProducto', mainRouter);
-app.use('./listadorProductos', mainRouter);
-app.use('./carrito', mainRouter);
-app.use('./creacion', mainRouter);
 
 app.listen(3000, ()=>console.log("Servidor en puerto 3000 corriendo"));
