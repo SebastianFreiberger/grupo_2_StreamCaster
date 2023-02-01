4a772f09150f9655d490ecf49d324da69bafd3b6
SebaFrei <sebastianfreiberger03@gmail.com>
2023-01-31 22:01:57 -0300

    agrego destacado a archivo json y traigo los destacados al home

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,6 +1,11 @@
+const path = require('path');
+const fs = require('fs');
+const productsJSON = path.join(__dirname,'../database/productos.json');
+const products = JSON.parse(fs.readFileSync(productsJSON, 'utf-8'));
+
 const controller = {
     home: (req, res) => {
-        res.render('home')
+        return res.render('home', {products: products});        
     },
 
     carrito: (req, res) => {
