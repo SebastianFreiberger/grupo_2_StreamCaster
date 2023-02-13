e43f3d378ed4171fd20dd5d439fa79fb74a600d3
fjmanzanares <fran.manzanares@gmail.com>
2023-02-13 18:53:58 -0300

    json de usuarios y rutas post de usuarios

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,30 @@
-const userController = {
+const path = require('path');
+const fs = require('fs');
+const usuariosJSON = path.join(__dirname, '../database/usuarios.json');
+const usuarios = JSON.parse(fs.readFileSync(usuariosJSON, 'utf-8'));
 
+
+
+const userController = {
+    // GET
     login: (req, res) => {
         res.render('login')
     },
-    
+    // GET    
     registro: (req, res) => {
         res.render('registro')
     },
+
+    registroPost: (req, res) => {
+        let id = usuarios[usuarios.length - 1].id + 1
+        let userNuevo = { id, ...req.body }
+        // userNuevo.avatar = req.file.filename;
+        usuarios.push(userNuevo)
+        fs.writeFileSync(usuariosJSON, JSON.stringify(usuarios, null, 2))
+        return res.redirect('/')
+    },
+    // registroPut: () => { },
+    // loginPost: () => { },
 
 }
 
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const userRouter = express.Router();
-
 const userController = require('../controllers/userController');
+const multer = require('multer');
+const path = require('path');
+
+const multerDiskStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, "../../public/images"));
+    },
+    filename: (req, file, cb) => {
+        let imageName = Date.now() + path.extname(file.originalname)
+        cb(null, imageName)
+    }
+})
+
+
+
 
 userRouter.get('/login', userController.login);
 userRouter.get('/registro', userController.registro);
-/* userRouter.get('/creacion', userController.creacionProducto);
-userRouter.get('/edicion', userController.edicionProducto); */
+userRouter.post('/registro', userController.registroPost);
+/* userRouter.put('/registro', userController.registroPut); */
 
 module.exports = userRouter;
