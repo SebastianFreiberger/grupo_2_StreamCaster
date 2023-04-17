de35cc46c7c052b572efb7cf648218e8cb18b4d5
fjmanzanares <fran.manzanares@gmail.com>
2023-04-17 19:02:52 -0300

    archivo js para validar front y vinculos en donde hay elementos form

---
+++ public/js/validacionesFront.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+
+    let formulario = document.querySelector("form")
+    formulario.addEventListener("submit", function(evento) {
+      evento.preventDefault()
+    })
+  
+  });
